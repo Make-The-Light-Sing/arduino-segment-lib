3df3d847cc1249896d94bf9d1e3bf1e53df83e27
Jeckel <jeckel@jeckel.fr>
2014-06-08 10:13:34 +0200

    Add example with a single segment configuration

---
+++ examples/SingleSegment/SingleSegment.ino
@@ -0,0 +1,50 @@
+/**
+ * Example with a single Segment configuration
+ *
+ * WARNING !! This library requires two additionnal libraries :
+ * - Color : https://github.com/jeckel/arduino-color-lib
+ * - FastSPI : https://github.com/jeckel/arduino-fastpsi_led-lib
+ *
+ * @author : Jeckel
+ * @link : https://github.com/jeckel/arduino-segment-lib
+ */
+#include <Color.h>
+#include <FastSPI_LED2.h>
+#include <Segment.h>
+
+#define LEDSTRIP_PIN 6
+#define NUM_LEDS     21
+#define NB_SEGMENT   1
+
+struct CRGB leds[NUM_LEDS];
+
+// Configure each segment with point to leds and segment length
+T_SegmentConfig seg_config[NB_SEGMENT] = {
+    { leds, NUM_LEDS}
+};
+
+// Effect link to segment : color, direction (up or down), and effect
+T_EffectConfig effect_config[NB_SEGMENT] = {
+    { CBlue, UP, Wave }
+};
+
+TM1809Controller800Mhz<LEDSTRIP_PIN> LED;
+SegmentCollection segments;
+
+void setup()
+{
+	Effect_Factory factory;
+    for(unsigned int i = 0; i < NB_SEGMENT; i++) {
+        segments.addSegment(new Segment(seg_config[i], factory.createEffect(effect_config[i])));
+    }
+    segments.init();
+    LED.init();
+}
+
+void loop()
+{
+    segments.preStep();
+    LED.showRGB((unsigned char *) leds, NUM_LEDS);
+    segments.postStep();
+    delay(20);
+}
